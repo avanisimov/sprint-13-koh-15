b7631cc56b3ee572ed753a9f964ef631ac92a5ee
Andrey <amdoit@yandex.ru>
2024-05-22 01:05:22 +0700

    #6 Добавлен пробел между ценой и знаком Р/шт

--- app/src/main/java/com/example/sprint13koh15/CatalogItemViewData.kt
+++ app/src/main/java/com/example/sprint13koh15/CatalogItemViewData.kt
@@ -33,7 +33,7 @@ class CatalogItemViewHolder(
             .load(viewData.item.imageUrl)
             .into(binding.image)
         binding.title.text = viewData.item.name
-        binding.price.text = "${getStringPrice(viewData.item.floatPrice)}Р/${viewData.item.unit}"
+        binding.price.text = "${getStringPrice(viewData.item.floatPrice)} Р/${viewData.item.unit}"
 
         if (viewData.count != null) {
             binding.addToCart.visibility = View.GONE
